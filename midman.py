bdce1fc4bde9a650c61f202159cf1893c97ca31b
Dingkun Guo <guodk@umich.edu>
2023-06-21 14:49:21 -0400

    test

--- midman.py
+++ midman.py
@@ -1,7 +1,7 @@
 from multiprocessing.connection import Listener, Client
 
 midman_address = ('128.2.205.54', 60888)
-server_address = ('10.1.1.110', 68888)
+server_address = ('10.1.1.110', 61888)
 
 def start_midman():
     with Listener(midman_address) as listener:
