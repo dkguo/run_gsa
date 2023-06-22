9b8a1aabc7fce0f0a40edbdb62f86ff8f477e53a
Dingkun Guo <guodk@umich.edu>
2023-06-22 15:04:22 -0400

    run gsa

--- run_predictors.py
+++ run_predictors.py
@@ -1,13 +1,17 @@
 import subprocess
+import sys
 
-processes = []
-for i in range(8):
-    process = subprocess.Popen(f'CUDA_VISIBLE_DEVICES={i} python predictor.py')
-    processes.append(process)
+if __name__ == '__main__':
+    num_gpus = int(sys.argv[1])
 
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    for process in processes:
-        process.kill()
+    processes = []
+    for i in range(num_gpus):
+        process = subprocess.Popen(f'CUDA_VISIBLE_DEVICES={i} python predictor.py')
+        processes.append(process)
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        for process in processes:
+            process.kill()
