8234af9638c6cc2f6f9ef4d4864c56a8973f29a9
Dingkun Guo <guodk@umich.edu>
2023-06-22 20:34:00 -0400

    run gsa

--- run_predictors.py
+++ run_predictors.py
@@ -1,13 +1,15 @@
 import subprocess
 import sys
 import time
+import torch
 
 if __name__ == '__main__':
-    num_gpus = int(sys.argv[1])
+    num_gpu = torch.cuda.device_count()
+    num_process = int(sys.argv[1])
 
     processes = []
-    for i in range(num_gpus):
-        process = subprocess.Popen(f'CUDA_VISIBLE_DEVICES={i} python predictor.py', shell=True)
+    for i in range(num_process):
+        process = subprocess.Popen(f'CUDA_VISIBLE_DEVICES={i % num_gpu} python predictor.py', shell=True)
         processes.append(process)
         time.sleep(2)
 
